d85ce075bebf685880544ce0986eebcaedae96f6
saravila <s.vilarinol@gmail.com>
2013-10-18 10:45:55 +0200

    Intento del test collide detect y overlap

--- 07-gameboard/spec/GameBoardSpec.js
+++ 07-gameboard/spec/GameBoardSpec.js
@@ -1,15 +1,15 @@
 /*
 
   En el anterior prototipo (06-player), el objeto Game permite
-  gestionar una colecciÛn de tableros (boards). Los tres campos de
+  gestionar una colecci√≥n de tableros (boards). Los tres campos de
   estrellas, la pantalla de inicio, y el sprite de la nave del
-  jugador, se aÒaden como tableros independientes para que Game pueda
-  ejecutar sus mÈtodos step() y draw() periÛdicamente desde su mÈtodo
+  jugador, se a√±aden como tableros independientes para que Game pueda
+  ejecutar sus m√©todos step() y draw() peri√≥dicamente desde su m√©todo
   loop(). Sin embargo los objetos que muestran los tableros no pueden
-  interaccionar entre sÌ. Aunque se aÒadiesen nuevos tableros para los
-  misiles y para los enemigos, resulta difÌcil con esta arquitectura
-  pensar en cÛmo podrÌa por ejemplo detectarse la colisiÛn de una nave
-  enemiga con la nave del jugador, o cÛmo podrÌa detectarse si un
+  interaccionar entre s√≠. Aunque se a√±adiesen nuevos tableros para los
+  misiles y para los enemigos, resulta dif√≠cil con esta arquitectura
+  pensar en c√≥mo podr√≠a por ejemplo detectarse la colisi√≥n de una nave
+  enemiga con la nave del jugador, o c√≥mo podr√≠a detectarse si un
   misil disparado por la nave del usuario ha colisionado con una nave
   enemiga.
 
@@ -17,42 +17,42 @@
   Requisitos:
 
   Este es precisamente el requisito que se ha identificado para este
-  prototipo: diseÒar e implementar un mecanismo que permita gestionar
-  la interacciÛn entre los elementos del juego. Para ello se diseÒar·
+  prototipo: dise√±ar e implementar un mecanismo que permita gestionar
+  la interacci√≥n entre los elementos del juego. Para ello se dise√±ar√°
   la clase GameBoard. Piensa en esta clase como un tablero de un juego
   de mesa, sobre el que se disponen los elementos del juego (fichas,
-  cartas, etc.). En Alien Invasion los elementos del juego ser·n las
+  cartas, etc.). En Alien Invasion los elementos del juego ser√°n las
   naves enemigas, la nave del jugador y los misiles. Para el objeto
-  Game, GameBoard ser· un board m·s, por lo que deber· ofrecer los
-  mÈtodos step() y draw(), siendo responsable de mostrar todos los
-  objetos que contenga cuando Game llame a estos mÈtodos.
+  Game, GameBoard ser√° un board m√°s, por lo que deber√° ofrecer los
+  m√©todos step() y draw(), siendo responsable de mostrar todos los
+  objetos que contenga cuando Game llame a estos m√©todos.
 
-  Este prototipo no aÒade funcionalidad nueva a la que ofrecÌa el
+  Este prototipo no a√±ade funcionalidad nueva a la que ofrec√≠a el
   prototipo 06.
 
 
-  EspecificaciÛn: GameBoard debe
+  Especificaci√≥n: GameBoard debe
 
-  - mantener una colecciÛn a la que se pueden aÒadir y de la que se
+  - mantener una colecci√≥n a la que se pueden a√±adir y de la que se
     pueden eliminar sprites como nave enemiga, misil, nave del
-    jugador, explosiÛn, etc.
+    jugador, explosi√≥n, etc.
 
-  - interacciÛn con Game: cuando Game llame a los mÈtodos step() y
-    draw() de un GameBoard que haya sido aÒadido como un board a Game,
-    GameBoard debe ocuparse de que se ejecuten los mÈtodos step() y
+  - interacci√≥n con Game: cuando Game llame a los m√©todos step() y
+    draw() de un GameBoard que haya sido a√±adido como un board a Game,
+    GameBoard debe ocuparse de que se ejecuten los m√©todos step() y
     draw() de todos los objetos que contenga
 
-  - debe ofrecer la posibilidad de detectar la colisiÛn entre
+  - debe ofrecer la posibilidad de detectar la colisi√≥n entre
     objetos. Un objeto sprite almacenado en GameBoard debe poder
     detectar si ha colisionado con otro objeto del mismo
-    GameBoard. Los misiles disparados por la nave del jugador deber·n
+    GameBoard. Los misiles disparados por la nave del jugador deber√°n
     poder detectar gracias a esta funcionalidad ofrecida por GameBoard
-    cu·ndo han colisionado con una nave enemiga; una nave enemiga debe
+    cu√°ndo han colisionado con una nave enemiga; una nave enemiga debe
     poder detectar si ha colisionado con la nave del jugador; un misil
     disparado por la nave enemiga debe poder detectar si ha
     colisionado con la nave del jugador. Para ello es necesario que se
-    pueda identificar de quÈ tipo es cada objeto sprite almacenado en
-    el tablero de juegos, pues cada objeto sÛlo quiere comprobar si ha
+    pueda identificar de qu√© tipo es cada objeto sprite almacenado en
+    el tablero de juegos, pues cada objeto s√≥lo quiere comprobar si ha
     colisionado con objetos de cierto tipo, no con todos los objetos.
 
 */
@@ -74,12 +74,14 @@ describe("Clase GameBoard", function(){
     it("Method add", function(){
       miBoard = new GameBoard();
       miPlayerShip = new PlayerShip();
+
       expect(miBoard.add(miPlayerShip)).toBe(miPlayerShip);
       expect(miBoard.objects.length).toBe(1); 
     });
 
     it ("Method reset removed", function() { 
       miBoard = new GameBoard(); 
+
       miBoard.resetRemoved(); 
       expect(miBoard.removed.length).toBe(0);
     });
@@ -87,6 +89,7 @@ describe("Clase GameBoard", function(){
     it ("Method remove", function() { 
       miBoard = new GameBoard(); 
       miPlayerShip = new PlayerShip();
+
       miBoard.resetRemoved();
       miBoard.remove(miPlayerShip); 
       expect(miBoard.removed.length).toBe(1);
@@ -95,6 +98,7 @@ describe("Clase GameBoard", function(){
      it ("Method finalizeRemoved", function() { 
       miBoard = new GameBoard(); 
       miPlayerShip = new PlayerShip();
+
       miBoard.resetRemoved();
       miBoard.remove(miPlayerShip);
       miBoard.finalizeRemoved();
@@ -104,10 +108,34 @@ describe("Clase GameBoard", function(){
     it ("Method iterate", function() { 
       miBoard = new GameBoard(); 
       miPlayerShip = new PlayerShip();
+
       miBoard.add(miPlayerShip);
       spyOn(miPlayerShip, "step");
       miBoard.iterate("step",1.0);
       expect(miPlayerShip.step).toHaveBeenCalled();
+    });  
+
+    it ("Method collide detect y overlap", function() {       
+      var misil = function () {
+        this.w = SpriteSheet.map['ship'].w;
+        this.h = SpriteSheet.map['ship'].h;
+        this.x = Game.width/2 - this.w / 2;
+        this.y = Game.height - 10 - this.h;        
+      };
+
+      miBoard = new GameBoard(); 
+      miPlayerShip = new PlayerShip(); 
+      miMisil = new misil();
+
+      miBoard.add(miPlayerShip);
+      miBoard.add(miMisil);
+      spyOn(miBoard, "detect"); 
+      spyOn(miBoard, "overlap");
+      miBoard.collide(miPlayerShip); 
+      expect(miBoard.detect).toHaveBeenCalled(); 
+      //expect(miBoard.collide(miPlayerShip)).toEqual(miMisil);
+      //expect(miBoard.overlap).toHaveBeenCalled();
+      expect(miBoard.overlap(miMisil,miPlayerShip)).toBeTruthy;
     });  
 
 });  
